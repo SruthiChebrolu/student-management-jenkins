d31fe8f132a5bfe03144a2315347d1bfb4cc5658
SruthiChebrolu <chvsaisruthi@gmail.com>
2026-07-25 18:30:01 -0500

    Add Calculator unit tests

--- src/main/java/com/practice/Calculator.java
+++ src/main/java/com/practice/Calculator.java
@@ -8,4 +8,7 @@ public class Calculator {
     public int subtract(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
+    public int multiply(int firstNumber, int secondNumber) {
+    return firstNumber * secondNumber;
+}
 }
---
+++ src/test/java/com/practice/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void shouldAddTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void shouldSubtractTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+}
